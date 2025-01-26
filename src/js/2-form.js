f38e585008b8493aa7c9233bb30845ac3445c107
PushEax <pusheax.vk@gmail.com>
2025-01-26 21:50:42 +0200

    restore data from storage, submit data and remove old data

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,10 +1,27 @@
-const formData = { email: '', message: '' };
+const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {
+  email: '',
+  message: '',
+};
 
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', e => {
-  formData[e.target.name] = e.target.value;
+form.elements.email.value = formData.email;
+form.elements.message.value = formData.message;
 
-  localStorage.setItem('form-data', JSON.stringify(formData));
-  console.log(localStorage);
+form.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    formData.email = '';
+    formData.message = '';
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+  }
 });
