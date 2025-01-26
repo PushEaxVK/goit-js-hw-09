ebcdd566f36192b25787dd892932ffffc593b9ce
PushEaxVK <98906196+PushEaxVK@users.noreply.github.com>
2025-01-26 19:30:47 +0000

    Deploying to gh-pages from @ PushEaxVK/goit-js-hw-09@70955042c64ee2d3329c6949fd0780e4cafbc40f 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t={email:"",message:""},o=document.querySelector(".feedback-form");o.addEventListener("input",e=>{t[e.target.name]=e.target.value,localStorage.setItem("form-data",JSON.stringify(t)),console.log(localStorage)});
+//# sourceMappingURL=2-form.js.map
